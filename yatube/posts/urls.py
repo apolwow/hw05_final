8d25f83a01d9341884f70ee10646ff1cb203c097
Alexey Polozhentsev <apolwow@gmail.com>
2021-08-04 08:40:43 +0500

    исправил код в соответсвии с pep8 в urls

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('<str:username>/unfollow/', views.profile_unfollow,
          name='profile_unfollow'),
     path('<str:username>/<int:post_id>/', views.post_view, name='post'),
-    path('<str:username>/<int:post_id>/comment', views.add_comment,
+    path('<str:username>/<int:post_id>/comment/', views.add_comment,
          name='add_comment'),
     path('<str:username>/<int:post_id>/edit/', views.post_edit,
          name='post_edit'),
